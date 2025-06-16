1a021b74c3d2367e46ff211b3fb07ae9afd810e4
Chunnan Yao <yaochunnan@gmail.com>
2025-06-16 02:45:51 -0700

    init rag

--- server/app/agent_config.py
+++ server/app/agent_config.py
@@ -4,9 +4,53 @@ from agents import Agent, WebSearchTool, function_tool
 from agents.tool import UserLocation
 
 import app.mock_api as mock_api
+import os
+import sys
+import time
+
+# Third-party imports
+import boto3
+from botocore.client import Config
+from botocore.exceptions import ClientError
+
+os.environ["AWS_DEFAULT_REGION"] = "us-east-2"
+
+# Print SDK versions
+print(f"Python version: {sys.version.split()[0]}")
+print(f"Boto3 SDK version: {boto3.__version__}")
+
+# Create boto3 session and set AWS region
+boto_session = boto3.Session()
+aws_region = boto_session.region_name
+
+print(f"aws regsion is {aws_region}")
+# Create boto3 clients for Bedrock
+bedrock_config = Config(connect_timeout=120, read_timeout=120, retries={'max_attempts': 0})
+bedrock_client = boto3.client('bedrock-runtime')
+bedrock_agent_client = boto3.client('bedrock-agent-runtime', config=bedrock_config)
+
+bedrock_kb_id = "W9CGK0RGBV"
+
+# Print configurations
+print("AWS Region:", aws_region)
+print("Bedrock Knowledge Base ID:", bedrock_kb_id)
+
+
+# Implement the `retrieve` function
+def retrieve(user_query, kb_id, num_of_results=5):
+    return bedrock_agent_client.retrieve(
+        retrievalQuery= {
+            'text': user_query
+        },
+        knowledgeBaseId=kb_id,
+        retrievalConfiguration= {
+            'vectorSearchConfiguration': {
+                'numberOfResults': num_of_results,
+            }
+        }
+    )
 
 STYLE_INSTRUCTIONS = "Use a conversational tone and write in a chat style without formal formatting or lists and do not use any emojis."
-
 
 @function_tool
 def get_past_orders():
@@ -16,7 +60,10 @@ def get_past_orders():
 @function_tool
 def submit_refund_request(order_number: str):
     """Confirm with the user first"""
-    return mock_api.submit_refund_request(order_number)
+    user_query = "How to use bluetooth?"
+
+    return retrieve(user_query, bedrock_kb_id, num_of_results=3)
+    # return mock_api.submit_refund_request(order_number)
 
 
 customer_support_agent = Agent(
