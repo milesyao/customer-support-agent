246f316379f71db21c12e1edbb8108f1b9ee02b1
Chunnan Yao <yaochunnan@gmail.com>
2025-06-20 02:15:38 -0700

    flush

--- server/server.py
+++ server/server.py
@@ -87,12 +87,12 @@ async def websocket_endpoint(websocket: WebSocket):
                 # Handle text based messages
                 if is_sync_message(message):
                     logger.info(f"new sync message: {message}")
-                    print(f"new sync message: {message}")
+                    print(f"new sync message: {message}", flush=True)
                     connection.history = message["inputs"]
                     if message.get("reset_agent", False):
                         connection.latest_agent = starting_agent
                 elif is_new_text_message(message):
-                    logger.info(f"new text message: {message}")
+                    logger.info(f"new text message: {message}", flush=True)
                     print(f"new text message: {message}")
                     user_input = process_inputs(message, connection)
                     async for new_output_tokens in workflow.run(user_input):
