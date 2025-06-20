ed85a2917a62e9d5ee0f0ca0c4668c63b071ff89
Chunnan Yao <yaochunnan@gmail.com>
2025-06-19 19:46:36 -0700

    simplify flow

--- server/app/agent_config.py
+++ server/app/agent_config.py
@@ -52,43 +52,42 @@ def retrieve(user_query, kb_id, num_of_results=5):
 
 STYLE_INSTRUCTIONS = "Use a conversational tone and write in a chat style without formal formatting or lists and do not use any emojis."
 
-@function_tool
-def get_past_orders():
-    return json.dumps(mock_api.get_past_orders())
+# @function_tool
+# def get_past_orders():
+#     return json.dumps(mock_api.get_past_orders())
+
+# @function_tool
+# def submit_refund_request(order_number: str):
+#     """Confirm with the user first"""
+#     return mock_api.submit_refund_request(order_number)
 
 @function_tool
-def submit_refund_request(order_number: str):
-    """Confirm with the user first"""
-    return mock_api.submit_refund_request(order_number)
-
-@function_tool
-def get_user_manual_info(user_query: str):
+def get_product_info(user_query: str):
     """find answers about user's questions on product details including usage, features, etc."""
     retrieved_results = retrieve(user_query, bedrock_kb_id, num_of_results=3)
-    print(f"retrieved_results: {retrieved_results}")
     return retrieved_results
 
 
 customer_support_agent = Agent(
     name="Customer Support Agent",
-    instructions=f"You are a customer support assistant. {STYLE_INSTRUCTIONS}",
+    instructions=f"You are a customer support assistant. Always use the get_product_info tool to answer user's questions. {STYLE_INSTRUCTIONS}",
     model="gpt-4o-mini",
-    tools=[get_past_orders, submit_refund_request, get_user_manual_info],
+    tools=[get_product_info],
 )
 
-stylist_agent = Agent(
-    name="Stylist Agent",
-    model="gpt-4o-mini",
-    instructions=f"You are a stylist assistant. {STYLE_INSTRUCTIONS}",
-    tools=[WebSearchTool(user_location=UserLocation(type="approximate", city="Tokyo"))],
-    handoffs=[customer_support_agent],
-)
+# stylist_agent = Agent(
+#     name="Stylist Agent",
+#     model="gpt-4o-mini",
+#     instructions=f"You are a stylist assistant. {STYLE_INSTRUCTIONS}",
+#     tools=[WebSearchTool(user_location=UserLocation(type="approximate", city="Tokyo"))],
+#     handoffs=[customer_support_agent],
+# )
 
-triage_agent = Agent(
-    name="Triage Agent",
-    model="gpt-4o-mini",
-    instructions=f"Route the user to the appropriate agent based on their request. {STYLE_INSTRUCTIONS}",
-    handoffs=[customer_support_agent],
-)
+# triage_agent = Agent(
+#     name="Triage Agent",
+#     model="gpt-4o-mini",
+#     instructions=f"Route the user to the appropriate agent based on their request. {STYLE_INSTRUCTIONS}",
+#     handoffs=[customer_support_agent],
+# )
 
-starting_agent = triage_agent
+starting_agent = customer_support_agent
