aace40784e439086da8317ab80112bdb70dc8c82
Chunnan Yao <yaochunnan@gmail.com>
2025-06-19 20:19:56 -0700

    simplify agents

--- server/app/agent_config.py
+++ server/app/agent_config.py
@@ -35,7 +35,6 @@ bedrock_kb_id = "W9CGK0RGBV"
 print("AWS Region:", aws_region)
 print("Bedrock Knowledge Base ID:", bedrock_kb_id)
 
-
 # Implement the `retrieve` function
 def retrieve(user_query, kb_id, num_of_results=5):
     return bedrock_agent_client.retrieve(
@@ -52,15 +51,6 @@ def retrieve(user_query, kb_id, num_of_results=5):
 
 STYLE_INSTRUCTIONS = "Use a conversational tone and write in a chat style without formal formatting or lists and do not use any emojis."
 
-# @function_tool
-# def get_past_orders():
-#     return json.dumps(mock_api.get_past_orders())
-
-# @function_tool
-# def submit_refund_request(order_number: str):
-#     """Confirm with the user first"""
-#     return mock_api.submit_refund_request(order_number)
-
 @function_tool
 def get_product_info(user_query: str):
     """find answers about user's questions on product details including usage, features, etc."""
@@ -74,20 +64,5 @@ customer_support_agent = Agent(
     model="gpt-4o-mini",
     tools=[get_product_info],
 )
-
-# stylist_agent = Agent(
-#     name="Stylist Agent",
-#     model="gpt-4o-mini",
-#     instructions=f"You are a stylist assistant. {STYLE_INSTRUCTIONS}",
-#     tools=[WebSearchTool(user_location=UserLocation(type="approximate", city="Tokyo"))],
-#     handoffs=[customer_support_agent],
-# )
-
-# triage_agent = Agent(
-#     name="Triage Agent",
-#     model="gpt-4o-mini",
-#     instructions=f"Route the user to the appropriate agent based on their request. {STYLE_INSTRUCTIONS}",
-#     handoffs=[customer_support_agent],
-# )
 
 starting_agent = customer_support_agent
